e00b62f4cb888e6a80d75ebaf019d0d386a52db9
SergioLangaritaBenitez <sergiolangaritabenitez@gmail.com>
2024-02-29 14:35:08 +0100

    new start

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/slangarita/Escritorio/dcnios/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/slangarita/Escritorio/dcnios/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/slangarita/Escritorio/dcnios/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/slangarita/Escritorio/dcnios/docs/src/css/custom.css"),
-];
